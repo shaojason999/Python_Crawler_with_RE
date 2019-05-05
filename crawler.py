0b4691095a67f833f17abad18179235a038b4913
shaojason999 <shaojason999@gmail.com>
2019-05-05 08:47:27 +0000

    Fixed task1: exclude the author's other names

--- crawler.py
+++ crawler.py
@@ -2,20 +2,29 @@ import urllib.request
 import re
 import matplotlib.pyplot as plt
 
-author = "Ian+Goodfellow"
+author = input()
+search_author=author.replace(' ','+')
+
+find_list = 'arxiv-result[\s\S]*?</li>'
+#find_title = 'title is-5 mathjax[\s\S]*?</p>'
+#find_year = 'originally announced[\s\S]*?</p>'
+find_author = '>[\s\S]*?</a>'
+
+temp_author_list = list()
+coauthor_list = list()
+
 error = list()
-start = 0
-find_title = 'title is-5 mathjax[\s\S]*?</p>'
-find_year = 'originally announced</span>[\s\S]*?</p>'
 year_ans = list()
 x = list()
 y = list()
+
+start = 0
 max_count = 0
 
 print("[ Author: " + author + " ]")
 
 while error == []:
-	url = "https://arxiv.org/search/?searchtype=author&query=" + author + "&abstracts=hide&size=50&order=-announced_date_first&start=" + str(start)
+	url = "https://arxiv.org/search/?searchtype=author&query=" + search_author + "&abstracts=hide&size=50&order=-announced_date_first&start=" + str(start)
 	content = urllib.request.urlopen(url)
 	html_str = content.read().decode('utf-8')
 	find_warning = "Sorry, your query for"
@@ -25,6 +34,7 @@ while error == []:
 	if error != [] :
 		print("\n[end of list]")
 
+		# draw the bar chart
 		max_year=max(year_ans)
 		max_year=int(max_year)
 		min_year=min(year_ans)
@@ -37,29 +47,53 @@ while error == []:
 			if count > max_count:
 				max_count = count
 			y.append(count)
-			x.append(i)
+			x.append(str(i))
 		# set the y-axis
 		plt.yticks(range(0,max_count+1,2))
 		plt.bar(x,y)
 		plt.show()
 	else:
-		# find the titles
-		result = re.findall(find_title, html_str)
+		#find the result lists
+		list_result = re.findall(find_list, html_str)
+		for l_r in list_result:
+			# find the coauthors
+			temp_author_list.clear()
+			authors = l_r.split("Authors:</span>")[1].split("</p>")[0].strip()
+			each_author = re.findall(find_author, authors)
+			for a_r in each_author:
+				result = a_r.split(">")[1].split("</a")[0].strip()
+				temp_author_list.append(result)
+			# find if the author is in the coauthor list
+			if author in temp_author_list:
+				coauthor_list.append(temp_author_list)
 
-		for r in result:
-			title=r.split("title is-5 mathjax\">")[1].split("</p>")[0].strip()
-			print(title)
+				# find the titles
+				title = l_r.split("title is-5 mathjax\">")[1].split("</p>")[0].strip()
+				print(title)
 
-		# find the originally announced years"
-		result = re.findall(find_year, html_str)
-
-		for r in result:
-			date = r.split("</span>")[1].split("</p>")[0].strip()
-			year = re.findall("[0-9]+", date)
-			year_ans.append(year[0])
-
+				# find the originally announced years"
+				date = l_r.split("originally announced</span>")[1].split("</p>")[0].strip()
+				year = re.findall("[0-9]+", date)
+				year_ans.append(year[0])
 
 		start=start+50
+
+#		# find the titles
+#		result = re.findall(find_title, html_str)
+#
+#		for r in result:
+#			title = r.split("title is-5 mathjax\">")[1].split("</p>")[0].strip()
+#			print(title)
+#
+#		# find the originally announced years"
+#		result = re.findall(find_year, html_str)
+#
+#		for r in result:
+#			date = r.split("</span>")[1].split("</p>")[0].strip()
+#			year = re.findall("[0-9]+", date)
+#			year_ans.append(year[0])
+#
+#		start=start+50
 
 
 
